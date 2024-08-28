0aade139dbfe27d76eb69709a676a8a993bb2db2
karhal <karhal@gmail.com>
2024-08-28 17:12:16 +0200

    fix: move the tmp creation folder on ready event

--- src/events/message-create.ts
+++ src/events/message-create.ts
@@ -50,7 +50,6 @@ export default class MessageCreate extends EventDiscord {
     if (response) {
       message.channel.send(response);
     }
-    FileHandler.createFolder(ConfigManager.config.tmpFolder.path);
     const attachmentsPath = FileHandler.getFolderFilenameFullPaths(
       ConfigManager.config.tmpFolder.path
     );
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,12 @@
 import EventDiscord from '../clients/events-discord';
 import { Events } from 'discord.js';
-
+import FileHandler from '../handlers/file-handler';
+import ConfigManager from '../configManager';
 export default class Ready extends EventDiscord {
   eventName = Events.ClientReady;
   once = true;
   handler = () => {
+    FileHandler.createFolder(ConfigManager.config.tmpFolder.path);
     console.log(`Ready! Logged in as ${this.discordClient?.user?.tag}`);
   };
 }
