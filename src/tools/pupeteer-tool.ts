06f113953cc602dc899b897c685bdc215d9064ea
karhal <karhal@gmail.com>
2025-01-08 15:20:15 +0100

    feat: web scrapper

---
+++ src/tools/pupeteer-tool.ts
@@ -0,0 +1,39 @@
+import ConfigManager from '../configManager';
+import AbstractTool from './absract-tool';
+import puppeteer from 'puppeteer';
+
+export default class PupeteerTool extends AbstractTool {
+  readonly toolName = 'pupeteer';
+  readonly isActivated = ConfigManager.config.pupeeteer.active;
+
+  readonly description =
+    'Use this tool to read the webpage and provide a summary of the content \
+    or to give user information about the webpage.';
+
+  readonly parameters = {
+    type: 'object',
+    properties: {
+      webpageUrl: { type: 'string' }
+    }
+  };
+
+  readonly execute = async (webpageUrl: string) => {
+    webpageUrl = JSON.parse(webpageUrl).webpageUrl;
+    const browser = await puppeteer.launch();
+    try {
+      const page = await browser.newPage();
+      await page.goto(webpageUrl);
+      const bodyContent = await page.evaluate(() => {
+        for (const script of document.body.querySelectorAll('script')) script.remove();
+        return document.body.innerHTML;
+      });
+      console.log(bodyContent);
+      await browser.close();
+      return { content: bodyContent };
+    }
+    catch (error) {
+      await browser.close();
+      return { error: 'Failed to fetch webpage content' };
+    }
+  };
+}
